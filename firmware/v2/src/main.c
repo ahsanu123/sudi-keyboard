a34827c6623e0c93a2b12ffe237bacea186975db
Ahsanu <caasperahsanuamala5@gmail.com>
2024-08-11 21:49:27 +0700

    finally fucking blinking nrf51822 working

--- firmware/v2/src/main.c
+++ firmware/v2/src/main.c
@@ -1,33 +1,27 @@
-
-#include "SEGGER_RTT.h"
 #include "boards.h"
-#include "nrf.h"
 #include "nrf_delay.h"
-#include "nrf_drv_gpiote.h"
-#include "nrf_drv_ppi.h"
-#include "nrf_drv_timer.h"
-#include "nrf_error.h"
 #include "nrf_gpio.h"
-#include "nrf_gpiote.h"
-#include "nrf_log.h"
-#include "nrf_log_backend.h"
-#include "nrf_log_ctrl.h"
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * @brief Function for application main entry.
+ */
 int main(void) {
-
-  NRF_LOG_INIT(NULL);
-  nrf_log_backend_init(false);
-  bsp_board_leds_init();
+  /* Configure board. */
+  // bsp_board_leds_init();
+  nrf_gpio_cfg_output(5);
 
   /* Toggle LEDs. */
-
   while (true) {
-    NRF_LOG_INFO("%s", nrf_log_push("sending data\r\n"));
-    nrf_delay_ms(500);
-    NRF_LOG_PROCESS();
+    for (int i = 0; i < LEDS_NUMBER; i++) {
+      nrf_gpio_pin_toggle(5);
+      // bsp_board_led_invert(i);
+      nrf_delay_ms(800);
+    }
   }
 }
 
-/** @} */
+/**
+ *@}
+ **/
