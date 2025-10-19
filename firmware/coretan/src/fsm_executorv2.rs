4779a6cf17920b4380bcb09a47c40e796c4ac276
Ahsanu <caasperahsanuamala5@gmail.com>
2025-10-19 09:21:48 +0700

    feat(test): basic test event loop work

--- firmware/coretan/src/fsm_executorv2.rs
+++ firmware/coretan/src/fsm_executorv2.rs
@@ -1,9 +1,6 @@
-use std::{
-    ops::Deref,
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum StateTypeEnum {
     Idle,
     OneClick,
@@ -20,6 +17,14 @@ pub struct StateChanger {
     pub conditions: Vec<Box<dyn Fn(&State) -> Option<State>>>,
 }
 
+impl std::fmt::Debug for StateChanger {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("StateChanger")
+            .field("conditions_len", &self.conditions.len())
+            .finish()
+    }
+}
+
 impl StateChangerTrait for StateChanger {
     fn handle_state(&self, state: &State) -> Option<State> {
         for condition in &self.conditions {
@@ -34,6 +39,7 @@ impl StateChangerTrait for StateChanger {
     }
 }
 
+// only change timestamp if state is changed
 impl StateChanger {
     fn new_to_idle() -> Self {
         Self {
@@ -43,7 +49,18 @@ impl StateChanger {
 
     fn new_to_one_click() -> Self {
         Self {
-            conditions: vec![Box::new(|state| None)],
+            conditions: vec![Box::new(|state| {
+                match state.state_type {
+                    StateTypeEnum::Idle => {
+                        if state.keys.is_some() {
+                            return Some(State::create_one_click(state.keys.clone()));
+                        }
+                    }
+                    _ => {}
+                }
+
+                None
+            })],
         }
     }
 
@@ -70,6 +87,7 @@ pub trait StateHandlerTrait {
     fn handle(&mut self);
 }
 
+#[derive(Debug)]
 pub struct State {
     pub state_type: StateTypeEnum,
     pub keys: Option<Vec<char>>,
@@ -107,10 +125,10 @@ impl State {
         }
     }
 
-    pub fn create_one_click() -> Self {
+    pub fn create_one_click(keys: Option<Vec<char>>) -> Self {
         Self {
             state_type: StateTypeEnum::OneClick,
-            keys: None,
+            keys,
             timestamp: Instant::now(),
             state_changers: vec![
                 StateChanger::new_to_double_click(),
@@ -158,12 +176,79 @@ impl Executor {
     }
 }
 
+impl Default for Executor {
+    fn default() -> Self {
+        Self {
+            current_state: State::create_idle(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod fsm_executorv2 {
+    use tokio::time::sleep;
+
     use super::*;
 
+    type EventOnTime = Box<dyn Fn(Duration) -> Option<Vec<char>>>;
+
+    fn change_event(duration: Duration, events: &[EventOnTime]) -> Option<Vec<char>> {
+        let mut current_condition: Option<Vec<char>> = None;
+        for event in events {
+            current_condition = event(duration);
+            if current_condition.is_some() {
+                break;
+            }
+        }
+
+        current_condition
+    }
+
     #[test]
-    fn test_idle() {
-        let idle_state = State::create_idle();
+    fn test_idle_to_one_click() {
+        let keys = Some(vec!['c']);
+
+        let mut executor = Executor::default();
+        executor.handle(keys);
+
+        let current_state = executor.current_state;
+
+        println!("{current_state:#?}");
+        assert!(matches!(current_state.state_type, StateTypeEnum::OneClick))
+    }
+
+    #[tokio::test]
+    async fn test_loop_events() {
+        let events_on_time: [EventOnTime; 1] = [Box::new(|duration| {
+            if duration < Duration::from_millis(10) {
+                return None;
+            } else if duration > Duration::from_millis(10) && duration <= Duration::from_millis(15)
+            {
+                return Some(vec!['c', 'a']);
+            } else if duration > Duration::from_millis(15) {
+                return None;
+            }
+            None
+        })];
+
+        let mut executor = Executor::default();
+        let mut current_keys: Option<Vec<char>> = None;
+        let start_time = Instant::now();
+
+        loop {
+            executor.handle(current_keys.clone());
+
+            sleep(Duration::from_millis(1)).await;
+
+            let elapsed_time = start_time.elapsed();
+            let current_state = &executor.current_state;
+            println!("{elapsed_time:?} => {current_state:?}");
+
+            current_keys = change_event(elapsed_time, &events_on_time);
+
+            if elapsed_time > Duration::from_millis(20) {
+                break;
+            }
+        }
     }
 }
